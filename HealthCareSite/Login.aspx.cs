a32d6bba3b0310e145317208bdb3eb7d063a4de4
max robins <tug78217@temple.edu>
2020-12-02 16:44:33 -0500

    login/register/scheduleApp

--- HealthCareSite/Login.aspx.cs
+++ HealthCareSite/Login.aspx.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Utilities;
 
 namespace HealthCareSite
 {
     public partial class Login : System.Web.UI.Page
     {
+        DBConnect objDB = new DBConnect();
+        SqlCommand objCommand = new SqlCommand();
+        string strSQL;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Homepage.aspx");
+        }
+
+        protected void Regsiter_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
         }
     }
 }
