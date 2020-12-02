1a49a2b294c378a40b449651889ff2b4b9fa7c4c
Anthony Nathan Littlejohn <tuh40168@temple.edu>
2020-12-02 17:16:32 -0500

    Added action button to gridview. Changed gridview so that datatable.net persist after post back

--- HealthCareSite/Homepage.aspx.designer.cs
+++ HealthCareSite/Homepage.aspx.designer.cs
@@ -85,6 +85,24 @@ namespace HealthCareSite {
         protected global::System.Web.UI.WebControls.Label lblType;
         
         /// <summary>
+        /// lblCard control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCard;
+        
+        /// <summary>
+        /// btnAppointment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAppointment;
+        
+        /// <summary>
         /// gvRecords control.
         /// </summary>
         /// <remarks>
